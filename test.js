d4dae46f6cf97c4ad0c8a2ed442c46214b0a4f30
Graham McGregor <graham@gmcgregor.ca>
2017-08-01 22:40:44 -0400

    Autoformat code using prettier

--- test.js
+++ test.js
@@ -5,7 +5,10 @@ const effects = Object.keys(a);
 
 for (const effect of effects) {
 	test(`throw error if invalid speed (${effect})`, t => {
-		t.throws(() => a[effect]('abc', ''), `Expected \`speed\` to be an number greater than 0`);
+		t.throws(
+			() => a[effect]('abc', ''),
+			`Expected \`speed\` to be an number greater than 0`
+		);
 	});
 
 	test(`doesn't throw (${effect})`, t => {
