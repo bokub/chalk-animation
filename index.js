d530711b692aac950e3a1afd02b554a00a21a61e
yTakkar <www.shtakkar@gmail.com>
2017-09-29 18:13:05 +0530

    Passed the test

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 'use strict';
 
-const
-	chalk = require('chalk'),
-	gradient = require('gradient-string');
+const chalk = require('chalk');
+const gradient = require('gradient-string');
 
 const log = console.log;
 let currentAnimation = null;
 
 const consoleFunctions = {
-	log: console.log.bind(console),
+	log: log.bind(console),
 	info: console.info.bind(console),
 	warn: console.warn.bind(console),
 	error: console.error.bind(console)
@@ -16,31 +15,28 @@ const consoleFunctions = {
 
 // eslint-disable-next-line guard-for-in
 for (const k in consoleFunctions) {
-	console[k] = () => {
+	console[k] = function () {
 		stopLastAnimation();
 		consoleFunctions[k].apply(console, arguments);
 	};
 }
 
-const
-	glitchChars = 'x*0987654321[]0-~@#(____!!!!\\|?????....0000\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t',
-	longHsv = {interpolation: 'hsv', hsvSpin: 'long'};
+const glitchChars = 'x*0987654321[]0-~@#(____!!!!\\|?????....0000\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t';
+const longHsv = {interpolation: 'hsv', hsvSpin: 'long'};
 
 const effects = {
-	rainbow = (str, frame) => {
-		const
-			hue = 5 * frame,
-			leftColor = {h: hue % 360, s: 1, v: 1},
-			rightColor = {h: (hue + 1) % 360, s: 1, v: 1};
+	rainbow(str, frame) {
+		const hue = 5 * frame;
+		const leftColor = {h: hue % 360, s: 1, v: 1};
+		const rightColor = {h: (hue + 1) % 360, s: 1, v: 1};
 		return gradient(leftColor, rightColor)(str, longHsv);
 	},
-	pulse = (str, frame) => {
+	pulse(str, frame) {
 		frame = (frame % 120) + 1;
-		const
-			transition = 6,
-			duration = 10,
-			on = '#ff1010',
-			off = '#e6e6e6';
+		const transition = 6;
+		const duration = 10;
+		const on = '#ff1010';
+		const off = '#e6e6e6';
 
 		if (frame >= (2 * transition) + duration) {
 			return chalk.hex(off)(str); // All white
@@ -66,14 +62,13 @@ const effects = {
 
 		return g(str);
 	},
-	glitch = (str, frame) => {
+	glitch(str, frame) {
 		if ((frame % 2) + (frame % 3) + (frame % 11) + (frame % 29) + (frame % 37) > 52) {
 			return str.replace(/[^\r\n]/g, ' ');
 		}
 
-		const
-			chunkSize = Math.max(3, Math.round(str.length * 0.02)),
-			chunks = [];
+		const chunkSize = Math.max(3, Math.round(str.length * 0.02));
+		const chunks = [];
 
 		for (let i = 0, length = str.length; i < length; i++) {
 			const skip = Math.round(Math.max(0, (Math.random() - 0.8) * chunkSize));
@@ -89,31 +84,42 @@ const effects = {
 		}
 
 		let result = chunks.join('');
-		result = Math.random() > 0.99 ? result.toUpperCase() : Math.random() < 0.01 ? result.toLowerCase() : null
+		const mr = Math.random();
+		if (mr > 0.99) {
+			result = result.toUpperCase();
+		} else if (mr < 0.01) {
+			result = result.toLowerCase();
+		}
+
 		return result;
 	},
-	radar = (str, frame) => {
-		const
-			depth = Math.floor(Math.min(str.length, str.length * 0.2)),
-			step = Math.floor(255 / depth),
-			globalPos = frame % (str.length + depth);
+	radar(str, frame) {
+		const mf = Math.floor;
+		const depth = mf(Math.min(str.length, str.length * 0.2));
+		const step = mf(255 / depth);
+
+		const globalPos = frame % (str.length + depth);
 
 		const chars = [];
 		for (let i = 0, length = str.length; i < length; i++) {
 			const pos = -(i - globalPos);
-			const shade = (depth - pos) * step;
-			pos > 0 && pos <= depth - 1 ? chars.push(chalk.rgb(shade, shade, shade)(str[i])) : chars.push(' ');
+			if (pos > 0 && pos <= depth - 1) {
+				const shade = (depth - pos) * step;
+				chars.push(chalk.rgb(shade, shade, shade)(str[i]));
+			} else {
+				chars.push(' ');
+			}
 		}
 
 		return chars.join('');
 	},
-	neon = (str, frame) => {
+	neon(str, frame) {
 		const color = (frame % 2 === 0) ? chalk.dim.rgb(88, 80, 85) : chalk.bold.rgb(213, 70, 242);
 		return color(str);
 	}
 };
 
-animateString = (str, effect, delay, speed) => {
+function animateString(str, effect, delay, speed) {
 	stopLastAnimation();
 
 	speed = speed === undefined ? 1 : parseFloat(speed);
@@ -125,17 +131,23 @@ animateString = (str, effect, delay, speed) => {
 		text: str,
 		stopped: false,
 		frame: 0,
-		nextStep = () => {
+		nextStep() {
 			const self = this;
 			log('\u001B[1F\u001B[G\u001B[2K' + effect(this.text, this.frame));
 			setTimeout(() => {
 				self.frame++;
-				!self.stopped ? self.nextStep() : null
+				if (!self.stopped) {
+					self.nextStep();
+				}
 			}, delay / speed);
 		},
-		replace = str => this.text = str,
-		stop = () => this.stopped = true,
-		start = () => {
+		replace(str) {
+			this.text = str;
+		},
+		stop() {
+			this.stopped = true;
+		},
+		start() {
 			this.stopped = false;
 			this.nextStep();
 		}
@@ -146,7 +158,11 @@ animateString = (str, effect, delay, speed) => {
 	return currentAnimation;
 }
 
-stopLastAnimation = () =>	currentAnimation ? currentAnimation.stop() : null
+function stopLastAnimation() {
+	if (currentAnimation) {
+		currentAnimation.stop();
+	}
+}
 
 module.exports = {
 	rainbow: (str, speed) => animateString(str, effects.rainbow, 15, speed),
@@ -154,4 +170,4 @@ module.exports = {
 	glitch: (str, speed) => animateString(str, effects.glitch, 55, speed),
 	radar: (str, speed) => animateString(str, effects.radar, 50, speed),
 	neon: (str, speed) => animateString(str, effects.neon, 500, speed)
-}
+};
